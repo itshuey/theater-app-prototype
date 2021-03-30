63c1bea5128b5d44a7206721c69018ec4626a3d8
itshuey <>
2021-03-29 19:52:46 -0700

    Adding demo assets

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -16,6 +16,7 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...Ionicons.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'wired': require('../assets/fonts/Wired-Regular.otf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
